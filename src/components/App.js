ff160b358a8b13ce76a5b8a8756e4a43094cd22f
Alejandro <alejandrochang10@gmail.com>
2018-12-21 15:12:43 -0800

    wiring up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const pageOne = () => {
-  return <div>PageOne
-    <Link to="page2">TO page 2</Link>
-  </div>
-}
-
-const pageTwo = () => {
-  return <div>pageTwo</div>
-}
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
-  return (
-    <BrowserRouter>
+  return <BrowserRouter>
       <div>
-          <Route path="/" exact component={pageOne} />
-          <Route path="/page2" component={pageTwo} />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
       </div>
-    </BrowserRouter>
-  );
+    </BrowserRouter>;
 }
 
 export default App;
---
+++ src/components/streams/StreamShow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StreamShow extends Component {
+  render() {
+    return (
+      <div>StreamShow</div>
+    );
+  }
+}
+
+export default StreamShow;
