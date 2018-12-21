a40a7dc357d94b95625d950898b1ee020af1c7c8
Alejandro <alejandrochang10@gmail.com>
2018-12-21 15:20:42 -0800

    connecting header to all components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Header from './Header';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -7,15 +8,22 @@ import StreamDelete from './streams/StreamDelete';
 import StreamShow from "./streams/StreamShow";
 
 const App = () => {
-  return <BrowserRouter>
+  return (
+    <div>
       <div>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" component={StreamCreate} />
-        <Route path="/streams/edit" component={StreamEdit} />
-        <Route path="/streams/delete" component={StreamDelete} />
-        <Route path="/streams/show" component={StreamShow} />
+        <Header />
       </div>
-    </BrowserRouter>;
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
