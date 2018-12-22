061d97631d63090fa404f6d12ccc6301574d7691
Alejandro <alejandrochang10@gmail.com>
2018-12-21 18:56:23 -0800

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div className="ui secondary pointing menu">
-    <Link to="/" className="item" >Home</Link>
+  return <div className="ui secondary pointing menu">
+      <Link to="/" style={{ color: "white" }} className="item">
+        Browse
+      </Link>
       <div className="right menu">
-        <Link to="/" className="item">All Streams</Link>
+        <Link to="/" style={{ color: "white" }} className="item">
+          All Streams
+        </Link>
       </div>
-    </div>
-  )
+    </div>;
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(
-    <App />
-  ,
+    <App />,
   document.getElementById("root")
 );
