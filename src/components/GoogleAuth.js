b6d21fe01e92b49bb022467921b1e23dcab965b2
Alejandro <alejandrochang10@gmail.com>
2018-12-22 23:00:55 -0800

    google buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -11,18 +11,34 @@ class GoogleAuth extends Component {
         scope: "email"
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange = () => {
+    // on change if the user's signs in or out
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  }
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if we're signed in</div>
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>
+      return (
+      <button className="ui red google button">
+        <i className="google icon" />
+        Sign Out
+      </button>
+      )
     } else {
-      return <div>I am not signed in</div>
+      return (
+        <button className="ui green google button">
+          <i className="google icon" />
+          Sign In
+      </button>
+      )
     }
   }
 
