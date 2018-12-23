b0a71e8d27c153b025e97bde18f9985de21f83fd
Alejandro <alejandrochang10@gmail.com>
2018-12-22 22:44:57 -0800

    adding methods and logic to GoogleAuth class

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
 
   componentDidMount() {
-    window.gapi.load('client:auth2', () => {
+    window.gapi.load("client:auth2", () => {
       window.gapi.client.init({
         clientId:
           "556438448285-l6ajm6f8crg4qa9tlac6c8i2t8guppki.apps.googleusercontent.com",
-        scope: 'email'
+        scope: "email"
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
       });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we're signed in</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am not signed in</div>
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
