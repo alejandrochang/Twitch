044fa3a0cc416d2bc10cecc1cf2eeaed9fd168a9
Alejandro <alejandrochang10@gmail.com>
2018-12-22 23:09:53 -0800

    google auth component complete

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -15,32 +15,40 @@ class GoogleAuth extends Component {
         this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
-  }
+  };
 
   onAuthChange = () => {
     // on change if the user's signs in or out
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-  }
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-      <button className="ui red google button">
+        <button onClick={this.onSignOut} className="ui red google button">
         <i className="google icon" />
         Sign Out
       </button>
       )
     } else {
       return (
-        <button className="ui green google button">
+        <button onClick={this.onSignIn} className="ui green google button">
           <i className="google icon" />
-          Sign In
+          Sign In with Google
       </button>
       )
     }
-  }
+  };
 
   render() {
     return <div>{this.renderAuthButton()}</div>;
