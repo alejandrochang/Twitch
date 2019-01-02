af2422fa25c79d2bf477175b48dc7d5555933a17
Alejandro <alejandrochang10@gmail.com>
2019-01-01 21:06:20 -0800

    form input tags rendering through helper method

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
+
+  renderInput() {
+    return <input />
+  }
+
   render() {
+    console.log(this.props);
     return (
-      <div>StreamCreate </div>
+      <form>
+        <Field name="Title" component={this.renderInput}/>
+        <Field name="Description" component={this.renderInput}/>
+      </form>
     );
   }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
+
+// through this form we get access to a ton of props, see them my console.log(this.props)
