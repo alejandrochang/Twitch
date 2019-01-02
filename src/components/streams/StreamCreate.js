14807e535bdf960d88a9e5d391a1423c7186b5c5
Alejandro <alejandrochang10@gmail.com>
2019-01-01 21:16:26 -0800

    spread operator for formInput

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-
-  renderInput() {
-    return <input />
+  renderInput(formProps) {
+    return <input {...formProps.input}/>
   }
 
   render() {
     console.log(this.props);
     return (
       <form>
-        <Field name="Title" component={this.renderInput}/>
-        <Field name="Description" component={this.renderInput}/>
+        <Field name="Title" component={this.renderInput} />
+        <Field name="Description" component={this.renderInput} />
       </form>
     );
   }
 }
 
 export default reduxForm({
-  form: 'streamCreate'
+  form: "streamCreate"
 })(StreamCreate);
 
 // through this form we get access to a ton of props, see them my console.log(this.props)
